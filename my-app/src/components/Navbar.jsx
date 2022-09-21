9e33e9f889e254dc95d9df96b6ece459870ac403
rybakovmax23 <rybakovmax23@gmail.com>
2022-09-21 14:05:41 +0300

    feat: add css modules

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from "react";
+import navbar from './Navbar.module.css';
 
-export const Navbar=()=>{
+export const Navbar = () => {
     return (
-      <nav className="nav">
-        <ul className="nav-menu">
-          <li className="nav-link">
-            <a href="#" className="link-anchor">
-              Profile
-            </a>
-          </li>
-          <li className="nav-link">
-            <a href="#" className="link-anchor">
-              Messages
-            </a>
-          </li>
-          <li className="nav-link">
-            <a href="#" className="link-anchor">
-              News
-            </a>
-          </li>
-          <li className="nav-link">
-            <a href="#" className="link-anchor">
-              Music
-            </a>
-          </li>
-        </ul>
-      </nav>
+        <nav className={navbar.nav}>
+            <ul className="nav-menu">
+                <li className="nav-link">
+                    <a href="#" className={`${navbar.link} ${navbar.active}`}>
+                        Profile
+                    </a>
+                </li>
+                <li className="nav-link">
+                    <a href="#" className={navbar.link}>
+                        Messages
+                    </a>
+                </li>
+                <li className="nav-link">
+                    <a href="#" className={navbar.link}>
+                        News
+                    </a>
+                </li>
+                <li className="nav-link">
+                    <a href="#" className={navbar.link}>
+                        Music
+                    </a>
+                </li>
+            </ul>
+        </nav>
     )
 }
--- my-app/src/components/Profile.jsx
+++ my-app/src/components/Profile.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import profile from'./Profile.module.css'
 
-export const Profile=()=>{
+export const Profile = () => {
     return (
-      <div className="content">
-        <img
-          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-          alt="pic"
-          className="picture"
-        />
-      </div>
+        <div className={profile.content}>
+            <div>
+            <img
+                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                alt="pic"
+                className="picture"
+            />
+            </div>
+            <div>
+                ava_description
+            </div>
+            <div>
+                Posts
+                <div>
+                    New Posts
+                </div>
+                <div className={profile.posts}>
+                    <div className={profile.item}>
+                        post1
+                    </div>
+                    <div className={profile.item}>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
